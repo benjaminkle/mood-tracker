c0450ab2099c21a0b229f1ad3fbd50a52edbb8c4
benjaminkle <benjamin.kle@hotmail.com>
2025-07-18 11:55:00 -0400

    Initial Design for Project, construct layout for index.html, script.js and styles.css

---
+++ script.js
@@ -0,0 +1,4 @@
+// Write function for saveMood, using localStorage to store data from users input
+// Write fetch function for fetchAdvice(mood) by searching advice for mood keyword
+// Write function for displayMood (moodHistory placeholder in html), display latest mood first in reverse order
+// Chart.js placeholder, consider visualize as line chart
